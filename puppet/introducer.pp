6270e3346664aadf8978f9b38d38fa22d59f0a9f
Cédric Jeanneret <tengu@ethack.org>
2014-11-16 11:41:08 +0100

    setup tor on introducer node

--- puppet/introducer.pp
+++ puppet/introducer.pp
@@ -8,29 +8,6 @@ Exec {
 include ::fogmail::base
 include ::fogmail::scripts
 include ::fogmail::tahoe::introducer
-
-class {'::tor':
-  relay                   => false,
-  nickname                => false,
-  publishserverdescriptor => false,
-  sockspolicies => [
-    {
-      policy => 'accept',
-      target => '127.0.0.1/8'
-    },
-    {
-      policy => 'reject',
-      target => '*',
-    },
-  ],
+class {'::fogmail::tor':
+  type => 'introducer',
 }
-
-file {'/var/lib/tahoe-lafs/introducer/introducer.port':
-  ensure  => file,
-  require => Class['fogmail::tahoe::introducer'],
-  mode    => '0600',
-  owner   => 'introducer',
-  group   => 'nogroup',
-  content => '59933',
-}
-
