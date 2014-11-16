4882138e6b7895e04624914f41f3df5c6c33cc80
Cédric Jeanneret <tengu@ethack.org>
2014-11-16 16:04:34 +0100

    set up firewall when we are not in Docker

--- puppet/modules/fogmail/manifests/tahoe/base.pp
+++ puppet/modules/fogmail/manifests/tahoe/base.pp
@@ -27,4 +27,50 @@ class fogmail::tahoe::base {
   }
 
   # TODO: python script mounting tahoe
+
+  if $::virtual != 'Docker' {
+    # iptables: forces above Tahoe users through Tor
+    class {'::firewall': }
+    Firewall {
+      table => 'nat',
+      chain => 'OUTPUT',
+      jump  => 'REDIRECT',
+    }
+    firewall {'000 redirect "client" DNS':
+      uid    => 'client',
+      proto  => 'udp',
+      dport  => 53,
+      todest => 5400,
+    }
+    firewall {'001 redirect "client" tcp':
+      uid      => 'client',
+      proto    => 'tcp',
+      outiface => 'eth0',
+      todest   => 9040,
+    }
+    firewall {'000 redirect "introducer" DNS':
+      uid    => 'introducer',
+      proto  => 'udp',
+      dport  => 53,
+      todest => 5400,
+    }
+    firewall {'001 redirect "introducer" tcp':
+      uid      => 'introducer',
+      proto    => 'tcp',
+      outiface => 'eth0',
+      todest   => 9040,
+    }
+    firewall {'000 redirect "storage" DNS':
+      uid    => 'storage',
+      proto  => 'udp',
+      dport  => 53,
+      todest => 5400,
+    }
+    firewall {'001 redirect "storage" tcp':
+      uid      => 'storage',
+      proto    => 'tcp',
+      outiface => 'eth0',
+      todest   => 9040,
+    }
+  }
 }
