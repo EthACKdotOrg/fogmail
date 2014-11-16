cfbef6471290cfc3c9a8946a00966a30ab348094
Cédric Jeanneret <tengu@ethack.org>
2014-11-16 12:48:25 +0100

    set tub.location so that we can grab onion hostname and inject in in Tahoe configuration. yipikai, mother fucker!

--- puppet/modules/fogmail/manifests/tahoe/ports.pp
+++ puppet/modules/fogmail/manifests/tahoe/ports.pp
@@ -2,15 +2,22 @@ define fogmail::tahoe::ports() {
   Ini_setting {
     path => "/var/lib/tahoe-lafs/${name}/tahoe.cfg",
   }
+  
+  $webPort = hiera('webPort')
+  $tubPort = hiera('tubPort')
 
   ini_setting {
     "set tub port for ${name}":
       section => 'node',
       setting => 'tub.port',
-      value   => hiera('tubPort');
+      value   => $tubPort;
+    "set tub URL for ${name}":
+      section => 'node',
+      setting => 'tub.location',
+      value   => "pb://CHANGEME:${tubport},127.0.0.1:${tubPort}";
     "set web port for ${name}":
       section => 'node',
       setting => 'web.port',
-      value   => hiera('webPort');
+      value   => $webPort;
   }
 }
